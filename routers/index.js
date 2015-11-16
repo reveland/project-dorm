05eaec3c0c45b0241b6551eaeef80458445b05aa
reveland <reveland92@gamil.com>
2015-11-16 11:17:31 +0000

    nothing

--- routers/index.js
+++ routers/index.js
@@ -10,4 +10,20 @@ router.get('/', function(req, res) {
     });
 });
 
+router.get('/addroom', function(req, res) {
+    req.app.models.room.count({}).exec(function countCB(error, roomCount) {
+        console.log('Created room with number ' + roomCount);
+        req.app.models.room.create({
+                number: roomCount
+            })
+            .then(function(room) {
+                req.flash('info', 'Szoba sikeresen felvéve!');
+                res.redirect('/');
+            })
+            .catch(function(err) {
+                console.log(err);
+            });
+    });
+});
+
 module.exports = router;
